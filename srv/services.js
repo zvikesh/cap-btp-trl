09e07cd75ad4b5eb11b149fe7cd3fb504bbe6960
Vikesh Sharma <k.vikeshsharma@gmail.com>
2026-09-14 20:21:47 +0000

    Add custom code

---
+++ srv/services.js
@@ -0,0 +1,24 @@
+const cds = require('@sap/cds')
+
+class ProcessorService extends cds.ApplicationService {
+  init() {
+
+    const { Incidents } = this.entities
+
+    // Reject changing status of a closed incident
+    this.before('UPDATE', Incidents, async req => {
+      const closed = await SELECT.one(1).from(req.subject).where`status.code = 'C'`
+      if (closed) req.reject`Can't modify a closed incident!`
+    })
+
+    // Flag incidents as urgent if their title contains the word 'urgent'
+    this.before(['CREATE', 'UPDATE'], Incidents, req => {
+      const urgent = req.data.title?.match(/urgent/i)
+      if (urgent) req.data.urgency_code = 'H'
+    })
+
+    return super.init()
+  }
+}
+
+module.exports = { ProcessorService }
